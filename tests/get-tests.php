48f4cd3c0ccbeaa0fad6fdfdfc76b61a9ecbc2ae
Joseph Scott <joseph@josephscott.org>
2025-02-01 12:35:57 -0700

    Tests against /json/

--- tests/get-tests.php
+++ tests/get-tests.php
@@ -5,9 +5,18 @@ beforeEach( function () {
 	$this->http = new \T7\HTTP\Request();
 } );
 
-test( 'get', function () {
+test( 'home', function () {
 	$response = $this->http->get( url: 'http://127.0.0.1:31313/' );
 
 	expect( $response->error )->toBe( false );
 	expect( $response->code )->toBe( 200 );
+} );
+
+test( 'json', function () {
+	$response = $this->http->get( url: 'http://127.0.0.1:31313/json/' );
+
+	expect( $response->error )->toBe( false );
+	expect( $response->code )->toBe( 200 );
+	expect( $response->headers['content-type'] )->toBe( 'application/json' );
+
 } );
