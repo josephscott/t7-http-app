2b2c101dbc8af5c688e4a11aa9bd388949edd289
Joseph Scott <joseph@josephscott.org>
2025-02-15 09:43:46 -0700

    POST tests

---
+++ tests/post-tests.php
@@ -0,0 +1,36 @@
+<?php
+declare( strict_types = 1 );
+
+beforeEach( function () {
+	$this->http = new \T7\HTTP\Request();
+} );
+
+test( 'form submission', function () {
+	$form_data = [
+		'name' => 'Test User',
+		'email' => 'test@example.com',
+		'message' => 'Hello, world!',
+	];
+
+	$response = $this->http->post(
+		url: 'http://127.0.0.1:31313/form/',
+		data: $form_data
+	);
+
+	expect( $response->error )->toBe( false );
+	expect( $response->code )->toBe( 200 );
+	expect( $response->headers['content-type'] )->toBe( 'application/json' );
+
+	$data = json_decode( $response->body, true );
+	expect( $data )->toHaveKey( 'success' );
+	expect( $data['success'] )->toBeTrue();
+	expect( $data )->toHaveKey( 'data' );
+	expect( $data['data'] )->toMatchArray( $form_data );
+} );
+
+test( 'post to get-only endpoint', function () {
+	$response = $this->http->post( url: 'http://127.0.0.1:31313/json/' );
+
+	expect( $response->error )->toBe( false );
+	expect( $response->code )->toBe( 405 );
+} );
